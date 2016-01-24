144551e1781cd7fa08e460843707441c027bb8a7
tomclarkson <athleticartistry@hotmail.com>
2016-01-24 22:27:21 +1300

    fix back not working in contest

--- components/BackButton.js
+++ components/BackButton.js
@@ -5,19 +5,23 @@ import ExRouter from 'ExRouter';
 
 export default class BackButton extends Component {
   goBack() {
-    let currentRoutes = this.props.navigator.getCurrentRoutes();
-    if([0,1].includes(currentRoutes.length)) {
-      return this.props.navigator.push(ExRouter.getDashboardRoute());
-    }
+    this.props.navigator.push(ExRouter.getDashboardRoute());
 
-    let previousRoute = currentRoutes[currentRoutes.length -2];
-    let wrappedComponent = previousRoute.scene.getWrappedInstance();
-    if(wrappedComponent) {
-      if(wrappedComponent.constructor.name == 'Onboarding') {
-        return this.props.navigator.push(ExRouter.getDashboardRoute());   
-      }
-    }
-    this.props.navigator.pop();
+    // the following code wasn't working inside of the exponent contest app for some reason
+
+    // let currentRoutes = this.props.navigator.getCurrentRoutes();
+    // if([0,1].includes(currentRoutes.length)) {
+    //   return this.props.navigator.push(ExRouter.getDashboardRoute());
+    // }
+
+    // let previousRoute = currentRoutes[currentRoutes.length -2];
+    // let wrappedComponent = previousRoute.scene.getWrappedInstance();
+    // if(wrappedComponent) {
+    //   if(wrappedComponent.constructor.name == 'Onboarding') {
+    //     return this.props.navigator.push(ExRouter.getDashboardRoute());   
+    //   }
+    // }
+    // this.props.navigator.pop();
   }
   render() {
     
